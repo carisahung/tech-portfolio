1999863e6fb199c8a114ec159ca82e0048036c2e
carisahung <72213501+carisahung@users.noreply.github.com>
2020-10-02 15:25:01 -0400

    Initial technical portfolio (for poka)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Poka the PUG!!', // Title for your website.
+    tagline: 'I know I look grumpy', //Ex: Continuously improving, aspiring technologist.
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://carisahung.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'carisahung',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'chao-yu-hung-b2581b138',
+        //twitter: '',
+        github: 'carisahung',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
